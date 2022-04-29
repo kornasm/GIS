e89c05d7e26e1b303d51cd9165205961ee90233d
Mikolaj Kornas <mikolaj.kornas@gmail.com>
2022-04-29 23:44:31 +0200

    Update players scraper
    Players scraper now get id and name. Results are saved to csv file

--- scrapers/PlayersScraper.py
+++ scrapers/PlayersScraper.py
@@ -3,21 +3,48 @@
 from lxml import html
 import lxml, lxml.html
 import requests
+import csv
+import time
 
 baseurl = 'https://fbref.com/en/players/'
 
-for firstletter in range(97, 123):
+a = 97
+z = 123
+filename = 'players'
+
+with open(filename , 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['id', 'name'])        
+
+for firstletter in range(a, z):
     char1 = chr(firstletter)
-    for secondletter in range(97, 123):
+    for secondletter in range(a, z):
         char2 = chr(secondletter)
+        print(char1 + char2)
+        time.sleep(10) # added to avoid being blocked by fbref server
         url = baseurl + char1 + char2 + '/'
+
         page = requests.get(url)
+
         tree = html.fromstring(page.content)
+        print('status code  ' + str(page.status_code))
 
-        players = tree.xpath('/html/body/div[@id="wrap"]/div[@id="content"]/div[@class="section_wrapper"]/div[@class="section_content"]/p/a/@href')
-        players.pop(-1) # empty record with the same path
+        playerids = tree.xpath('/html/body/div[@id="wrap"]/div[@id="content"]/div[@class="section_wrapper"]/div[@class="section_content"]/p/a//@href') # id
+        playernames = tree.xpath('/html/body/div[@id="wrap"]/div[@id="content"]/div[@class="section_wrapper"]/div[@class="section_content"]/p/a//text()') # name
 
-        with open('players' , 'w') as file:
-            for player in players:
-                file.write(player)
-                file.write("\n")
+        if(len(playerids) > 0):
+            playernames.pop(-1) # empty record with the same path
+            playerids.pop(-1)
+
+        for i in range(0,len(playerids)):
+            playerlink = str(playerids[i]).split('/')
+            playerids[i] = playerlink[3]
+
+        players = list(zip(playerids, playernames))
+
+        with open(filename , 'a') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerows(players)
+
+        print('no players  ' + str(len(playerids)))
+        print('')
